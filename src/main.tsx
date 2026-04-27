0c60ef198360cdd2f04dd6e9609b159b64b947f2
dhruv-chothani <dhruvchothani55@gmail.com>
2026-04-27 10:50:50 +0530

    Add more sample products to localStorage service and initialize on app start

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import localStorageService from "./services/localStorageService";
+
+// Initialize localStorage with sample data if empty
+localStorageService.initializeData();
 
 createRoot(document.getElementById("root")!).render(<App />);
